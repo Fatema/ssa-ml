6484ecf45f3e59633a5dd931ea12d4080659dc7c
FAlkhanaizi <fatema.alkhanaizi@durham.ac.uk>
2019-03-22 11:10:28 +0000

    made a monster

--- vae_pegasus.py
+++ vae_pegasus.py
@@ -11,9 +11,13 @@ import re
 # local version imports
 import visdom
 vis = visdom.Visdom(server='ncc1.clients.dur.ac.uk',port=12345)
-vis.line(X=np.array([0]), Y=np.array([[np.nan]]), win='loss')
+vis.line(X=np.array([0]), Y=np.array([[np.nan,np.nan,np.nan]]), win='loss')
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+ENCODER_MODEL_PATH = 'encoder_model.plk'
+DECODER_MODEL_PATH = 'decoder_model.plk'
+DISCRIMINATOR_MODEL_PATH = 'discriminator_model.plk'
 
 # helper function to make getting another batch of data easier
 def cycle(iterable):
@@ -60,29 +64,18 @@ test_iterator = iter(cycle(test_loader))
 print(f'> Size of training dataset {len(train_loader.dataset)}')
 print(f'> Size of test dataset {len(test_loader.dataset)}')
 
-# define variational autoencoder model
-class VAE(nn.Module):
+# define variational autoencoder components along with a discriminator models
+class Encoder(nn.Module):
     def __init__(self, intermediate_size=128, hidden_size=20):
-        super(VAE, self).__init__()
-
-        # encoder
+        super(Encoder, self).__init__()
         self.conv1 = nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(3, 32, kernel_size=2, stride=2, padding=0)
         self.conv3 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
         self.conv4 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
         self.fc1 = nn.Linear(16 * 16 * 32, intermediate_size)
 
-        # latent space
         self.fc21 = nn.Linear(intermediate_size, hidden_size)
         self.fc22 = nn.Linear(intermediate_size, hidden_size)
-
-        # decoder
-        self.fc3 = nn.Linear(hidden_size, intermediate_size)
-        self.fc4 = nn.Linear(intermediate_size, 8192)
-        self.deconv1 = nn.ConvTranspose2d(32, 32, kernel_size=3, stride=1, padding=1)
-        self.deconv2 = nn.ConvTranspose2d(32, 32, kernel_size=3, stride=1, padding=1)
-        self.deconv3 = nn.ConvTranspose2d(32, 32, kernel_size=2, stride=2, padding=0)
-        self.conv5 = nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1)
 
     def encode(self, x):
         out = F.relu(self.conv1(x))
@@ -101,6 +94,22 @@ class VAE(nn.Module):
         else:
             return mu
 
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return z, mu, logvar
+
+class Decoder(nn.Module):
+    def __init__(self, intermediate_size=128, hidden_size=20):
+        super(Decoder, self).__init__()
+
+        self.fc3 = nn.Linear(hidden_size, intermediate_size)
+        self.fc4 = nn.Linear(intermediate_size, 8192)
+        self.deconv1 = nn.ConvTranspose2d(32, 32, kernel_size=3, stride=1, padding=1)
+        self.deconv2 = nn.ConvTranspose2d(32, 32, kernel_size=3, stride=1, padding=1)
+        self.deconv3 = nn.ConvTranspose2d(32, 32, kernel_size=2, stride=2, padding=0)
+        self.conv5 = nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1)
+
     def decode(self, z):
         h3  = F.relu(self.fc3(z))
         out = F.relu(self.fc4(h3))
@@ -111,20 +120,45 @@ class VAE(nn.Module):
         out = torch.sigmoid(self.conv5(out))
         return out
 
-    def forward(self, x):
-        mu, logvar = self.encode(x)
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
+    def forward(self, z):
+        return self.decode(z)
 
+class Discriminator(nn.Module):
+    def __init__(self, f=64):
+        super(Discriminator, self).__init__()
+        self.discriminate = nn.Sequential(
+            nn.Conv2d(3, f, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(f, f*2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(f*2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(f*2, f*4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(f*4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(f*4, f*8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(f*8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(f*8, 1, 4, 2, 1, bias=False),
+            nn.Sigmoid()
+        )
 
-N = VAE().to(device)
+N_Encoder = Encoder().to(device)
+N_Decoder = Decoder().to(device)
+N_Discriminator = Discriminator().to(device)
+
 try:
-    N.load_state_dict(torch.load(MODEL_PATH))
-    N.eval()
+    N_Encoder.load_state_dict(torch.load(ENCODER_MODEL_PATH))
+    N_Encoder.eval()
+    N_Decoder.load_state_dict(torch.load(DECODER_MODEL_PATH))
+    N_Decoder.eval()
+    N_Discriminator.load_state_dict(torch.load(DISCRIMINATOR_MODEL_PATH))
+    N_Discriminator.eval()
 except:
     print('no model found')
 
-optimiser = torch.optim.Adam(N.parameters(), lr=0.001)
+optimiser_encoder = torch.optim.Adam(N_Encoder.parameters(), lr=0.001)
+optimiser_decoder = torch.optim.Adam(N_Decoder.parameters(), lr=0.001)
+optimiser_discriminator = torch.optim.Adam(N_Discriminator.parameters(), lr=0.001)
 
 epoch = 0
 
@@ -138,7 +172,9 @@ def vae_loss(p, x, mu, logvar):
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-    return BCE + KLD
+    return BCE, KLD
+
+lambda_bce = 1e-6
 
 # main training loop
 # training loop
@@ -146,7 +182,9 @@ while (epoch < 100):
     
     # arrays for metrics
     logs = {}
-    train_loss_arr = np.zeros(0)
+    train_loss_encoder_arr = np.zeros(0)
+    train_loss_decoder_arr = np.zeros(0)
+    train_loss_discriminator_arr = np.zeros(0)
 
     # iterate over some of the train dateset
     for i in range(1000):
@@ -155,24 +193,38 @@ while (epoch < 100):
 
         optimiser.zero_grad()
         
-        p, mu, logvar = N(x)
-        loss = vae_loss(p, x, mu, logvar)
-        loss.backward()
-        optimiser.step()
+        z, mu, logvar = N_Encoder(x)
+        p = N_Decoder(z)
 
-        train_loss_arr = np.append(train_loss_arr, loss.cpu().data)
+        loss_bce, loss_kl = vae_loss(p, x, mu, logvar)
+
+        loss_encoder = torch.sum(loss_kl) + torch.sum(loss_bce)
+        l_r = bce_loss(D.discriminate(x).mean(), torch.ones(1)[0].to(device)) # real -> 1
+        l_f = bce_loss(D.discriminate(p).mean(), torch.zeros(1)[0].to(device)) #  fake -> 0
+        loss_discriminator = (l_r + l_f)/2.0
+
+        loss_decoder = torch.sum(lambda_bce * loss_bce) - (1.0 - loss_bce) * loss_discriminator
+
+        loss_encoder.backward()
+        loss_decoder.backward()
+        loss_discriminator.backward()
+
+        optimiser_encoder.step()
+        optimiser_decoder.step()
+        optimiser_discriminator.step()
+
+        train_loss_encoder_arr = np.append(train_loss_encoder_arr, loss_encoder.cpu().data)
+        train_loss_decoder_arr = np.append(train_loss_decoder_arr, loss_decoder.cpu().data)
+        train_loss_discriminator_arr = np.append(train_loss_discriminator_arr, loss_discriminator.cpu().data)
 
     example_1 = (test_loader.dataset[13][0]).to(device)  # horse
     example_2 = (test_loader.dataset[160][0]).to(device) # bird
 
-    ex1_mu, ex1_logvar = N.encode(example_1.unsqueeze(0))
-    ex1_z = N.reparameterize(ex1_mu, ex1_logvar)
-
-    ex2_mu, ex2_logvar = N.encode(example_2.unsqueeze(0))
-    ex2_z = N.reparameterize(ex2_mu, ex2_logvar)
+    ex1_z, ex1_mu, ex1_logvar = N_Encoder(example_1.unsqueeze(0))
+    ex2_z, ex2_mu, ex2_logvar = N_Encoder(example_2.unsqueeze(0))
 
     # this is some sad blurry excuse of a Pegasus, hopefully you can make a better one
-    bad_pegasus = N.decode(0.9*ex1_z + 0.1*ex2_z).squeeze(0)
+    bad_pegasus = N_Decoder(0.9*ex1_z + 0.1*ex2_z).squeeze(0)
 
     pegasus = bad_pegasus.cpu().data.permute(0,2,1).contiguous().permute(2,1,0)
 
@@ -180,13 +232,19 @@ while (epoch < 100):
         pegasus.numpy().T
     )
 
-    torch.save(copy.deepcopy(N.state_dict()), 'vae_pegasus_model.pkl')
+    torch.save(copy.deepcopy(N.state_dict()), 'encoder_model.pkl')
+    torch.save(copy.deepcopy(N.state_dict()), 'decoder_model.pkl')
+    torch.save(copy.deepcopy(N.state_dict()), 'discriminator_model.pkl')
 
     # plot metrics
     vis.line(X=np.array([epoch]), Y=np.array([[
-        train_loss_arr.mean()
+        train_loss_encoder_arr.mean(),
+        train_loss_decoder_arr.mean(),
+        train_loss_discriminator_arr.mean(),
     ]]), win='loss', opts=dict(title='loss', xlabel='epoch', ylabel='loss', ytype='log', legend=[
-        'train loss'
+        'encoder loss',
+        'decoder loss',
+        'discriminator loss'
     ]), update='append')
 
     epoch = epoch + 1
@@ -194,14 +252,11 @@ while (epoch < 100):
 example_1 = (test_loader.dataset[13][0]).to(device)  # horse
 example_2 = (test_loader.dataset[160][0]).to(device) # bird
 
-ex1_mu, ex1_logvar = N.encode(example_1.unsqueeze(0))
-ex1_z = N.reparameterize(ex1_mu, ex1_logvar)
-
-ex2_mu, ex2_logvar = N.encode(example_2.unsqueeze(0))
-ex2_z = N.reparameterize(ex2_mu, ex2_logvar)
+ex1_z, ex1_mu, ex1_logvar = N_Encoder(example_1.unsqueeze(0))
+ex2_z, ex2_mu, ex2_logvar = N_Encoder(example_2.unsqueeze(0))
 
 # this is some sad blurry excuse of a Pegasus, hopefully you can make a better one
-bad_pegasus = N.decode(0.9*ex1_z + 0.1*ex2_z).squeeze(0)
+bad_pegasus = N_Decoder(0.9*ex1_z + 0.1*ex2_z).squeeze(0)
 
 pegasus = bad_pegasus.cpu().data.permute(0,2,1).contiguous().permute(2,1,0)
 
